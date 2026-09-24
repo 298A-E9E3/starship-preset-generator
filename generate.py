1682bc5fa59d8353697a71ef226b7b4880acc58f
Atlas_phone <iblacksushi@gmail.com>
2026-09-24 12:03:03 -0400

    Add comments

--- generate.py
+++ generate.py
@@ -4,6 +4,7 @@ import time
 import re
 from typing import Any, Dict
 
+# Source: https://github.com/toml-lang/toml/discussions/1011
 def merge_dicts(a: Dict[Any, Any], b: Dict[Any, Any]) -> Dict[Any, Any]:
     """Merge two dictionaries recursively.
 
@@ -20,20 +21,19 @@ def merge_dicts(a: Dict[Any, Any], b: Dict[Any, Any]) -> Dict[Any, Any]:
     return result
 
 
-#Define indices. Not in an enum for ease of use
+# Define indices. Not in an enum for ease of use
 FG = 0
 BG = 1
 
+# Get base presets
 subprocess.run(["starship", "preset", "nerd-font-symbols", "-o", "./nfs.toml"])
 subprocess.run(["starship", "preset", "bracketed-segments", "-o", "./bracketed.toml"])
-
 nfsDict = tomlkit.parse(open("./nfs.toml", "r").read())
 bsDict = tomlkit.parse(open("./bracketed.toml", "r").read())
-#configStr = subprocess.run(["tomlq", "-t", "-s", "'.[0] * .[1]'", "./nfs.toml", "./bracketed.toml"], capture_output=True, text=True).stdout
-#print(configStr)
+# Merge base presets
 config = merge_dicts(nfsDict, bsDict)
-#config = tomlkit.parse(configStr)
 
+# Define segment colors - first element is fg, second is bg
 segmentColors = [
     ("#000000", "#a3aed2"),
     ("#e3e5e5", "#769ff0"),
@@ -41,8 +41,10 @@ segmentColors = [
     ("#769ff0", "#212736"),
     ("#a0a9cb", "#1d2230")
 ]
+
+# Define segment contents
 segmentContents = {
-        "info":      ["$username"],
+    "info":      ["$username"],
     "directory": [],
     "git":       ["$git_branch", "$git_status"],
     "lang":      ["$c", "$cpp", "$rust", "$golang", "$nodejs", "$bun", "$php", "$java", "$kotlin", "$haskell", "$python"],
@@ -53,44 +55,63 @@ segmentContents = {
 }
 # List of modules that have dynamic colors, and thus should not have an fg style set
 dynColorModules = []
+# The order of the segments. The list should not be longer than segmentColors
 segmentOrder = ["info", "git", "lang", "sys-stats"]
+assert len(segmentOrder) <= len(segmentColors)
 
 # Generate format and apply styles
+# bracket regex
 breg = re.compile(r"\[(.*)\]")
+# Initialize the shell format string
 shellFormat = f"[]({segmentColors[0][1]})"
 for i in range(len(segmentOrder)):
     segment = segmentOrder[i]
     contents = segmentContents[segment]
     colors = segmentColors[i]
+    # Loop through segment contents and add them to the format
     for j in range(len(contents)):
         shellFormat += contents[j]
-        moduleName = contents[j][1::]
-        if(moduleName in ["os", "memory_usage"]):
-            config[moduleName]["disabled"] = False
+        # Various pieces of logic that apply only to modules
+        if(contents[j].startswith("$")):
+            # Strip the starting $ to get the module's name
+            moduleName = contents[j][1::]
 
-        if(not moduleName in dynColorModules):
-            styleStr = f"fg:{colors[FG]} bg:{colors[BG]}"
-        else:
-            styleStr = f"bg:{colors[BG]}"
+            # Re-enable modules that are disabled by default
+            if(moduleName in ["os", "memory_usage"]):
+                config[moduleName]["disabled"] = False
 
-        config[moduleName]["style"] = styleStr
-        try:
-            moduleFormat: str = config[moduleName]["format"]
-            moduleFormat = moduleFormat.replace("\\[", "").replace("\\]", "")
-            moduleFormat = breg.sub(r"[[ \1 ]("+styleStr+")]", moduleFormat)
-            config[moduleName]["format"] = moduleFormat
-        except:
-            if(moduleName=="directory"):
-                config[moduleName]["format"] = "[ $path ]($style)"
-            pass
+            # Generate the style string. fg is only defined if the module isn't dynamically colored
+            if(not moduleName in dynColorModules):
+                styleStr = f"fg:{colors[FG]} bg:{colors[BG]}"
+            else:
+                styleStr = f"bg:{colors[BG]}"
 
+            config[moduleName]["style"] = styleStr
+
+            # Modify the module's format
+            try:
+                moduleFormat: str = config[moduleName]["format"]
+                moduleFormat = moduleFormat.replace("\\[", "").replace("\\]", "")
+                moduleFormat = breg.sub(r"[[ \1 ]("+styleStr+")]", moduleFormat)
+                config[moduleName]["format"] = moduleFormat
+            except:
+                # Apply a format to the directory module, which doesn't have one by default
+                if(moduleName=="directory"):
+                    config[moduleName]["format"] = "[ $path ]($style)"
+                # Gracefully fail if the module doesn't have a format defined
+                pass
+
+    # Cap off the segment
     bgStr = f"bg:{segmentColors[i+1][1]}" if i < len(segmentOrder) - 1 else ""
     shellFormat += f"[{'' if i < len(segmentOrder) -1 else ''}](fg:{segmentColors[i][1]} {bgStr})"
 
+# Add the prompt
 shellFormat += "$line_break"
 for i in range(len(segmentContents["prompt"])):
     shellFormat += segmentContents["prompt"][i]
 
+# Apply the format string
 config["format"] = shellFormat
 
+# Output the final config
 print(tomlkit.dumps(config))
